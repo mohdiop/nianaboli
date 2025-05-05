38f3ccf3b0ea9499415c1d03a980ea7f1a806e82
Mohamed Diop <mohdiopcode@gmail.com>
2025-05-05 17:51:48 +0000

    Crée la table groupe

--- connexion.py
+++ connexion.py
@@ -6,3 +6,6 @@ con.autocommit = True
 def initialize():
     con.execute("CREATE TABLE IF NOT EXISTS " \
     "utilisateur (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, prenom TEXT, email TEXT UNIQUE, motDePasse TEXT)")
+    con.execute("CREATE TABLE IF NOT EXISTS " \
+    "groupe (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, dateCreation TEXT, idUtilisateur INTEGER, " \
+    "FOREIGN KEY(idUtilisateur) REFERENCES utilisateur(id))")
