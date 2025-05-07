97f0c3cc11b0544a8e560425f85fd721d0c4a9ca
Mohamed Toure <berrahtech1995@gmail.com>
2025-05-07 10:08:25 +0000

    Ajoute la fonctionnalité de login

---
+++ connectUser.py
@@ -0,0 +1,72 @@
+import connexion, createUser, sys
+
+def login(choix):
+    if(choix == "c"):
+        print("Connexion\n\n")
+        telephone = input("Votre numéro de téléphone: ")
+        user = createUser.getUserByTel(telephone)
+        while(user is None):
+            telephone = input("Utilisateur introuvable! \nVérifiez votre numéro de téléphone: ")
+            user = createUser.getUserByTel(telephone)
+
+        motDePasse = input("Votre mot de passe: ")
+        i = 0
+        while(motDePasse != user.motDePasse):
+            i = i+1
+            motDePasse = input(f"Mot de passe incorrect! Tentative restant : {5-i} \nVotre mot de passe: ")
+            if(i == 4):
+                sys.exit("Nombre de tentative dépassé")
+
+        print(f"Bienvenue {user.prenom} {user.nom} :)")
+        return True
+
+# query = "utilisateur(Telephone VARCHAR UNIQUE, Password VARCHAR)"
+
+# cursor.execute(query)
+# conn.commit()
+
+# def enter(telephone, password):
+#     query = "INSERT INTO utilisateur (Telephone, Password) VALUES (?, ?)"
+#     cursor.execute(query, (telephone, password))
+#     conn.commit()
+
+# def check(telephone, password):
+#     query = 'SELECT * FROM utilisateur WHERE Telephone = ? AND Password = ?'
+#     cursor.execute(query, (telephone, password))
+#     result = cursor.fetchone()
+#     conn.commit()
+#     print('[DEBUG][check] result:', result)
+#     return result
+
+# def loginlol():
+#     answer = input("Login (Y/N): ")
+
+#     if answer.lower() == "y":
+#         telephone = input("Telephone: ")
+#         password = input("Password: ")
+#         if check(telephone, password):
+#             print("Telephone correct!")
+#             print("Password correct!")
+#             print("Logging in...")
+#         else:
+#             print("Something wrong")
+
+# # --- main ---
+
+# conn = sqlite3.connect("nianaboli.db")
+# cursor = conn.cursor()
+
+# #create_table()
+
+# Telephone = input("Create telephone: ")
+# Password = input("Create password: ")
+
+# enter(Telephone, Password)
+
+# #check(Telephone, Password)
+
+# loginlol()
+
+# cursor.close()
+# conn.close()
+    
