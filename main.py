c05a2801ae8032e415bd1348fb8166a0a4bc00ef
Mohamed Diop <mohdiopcode@gmail.com>
2025-05-07 12:09:30 +0000

    Ajoute la déconnexion

--- main.py
+++ main.py
@@ -3,29 +3,33 @@ import connexion, createUser, connectUser, models, creationgroupe
 connexion.initialize()
 print("------------- Bienvenue sur Nianaboli, votre console de gestion de dépenses collaboratives -------------")
 
-choix = 0
-while(choix not in (1, 2)):
-    choix = int(input("1.) Connexion\n2.) Inscription\n"))
+def authentification():
+    choix = 0
+    while(choix not in (1, 2)):
+        choix = int(input("1.) Connexion\n2.) Inscription\n"))
 
-user = None
+    user = None
 
-if(choix == 1):
-    user = connectUser.login()
-elif(choix == 2):
-    user = createUser.creationProcess()
+    if(choix == 1):
+        user = connectUser.login()
+    elif(choix == 2):
+        user = createUser.creationProcess()
+    menuPrincipal(user)
 
 def menuPrincipal(utilisateur: models.UtilisateurInfo):
     print(f"\nBienvenue {utilisateur.prenom} {utilisateur.nom} :)\n")
     print("----------------------------------------------------------------------------")
-    print("\n1.) Créer un groupe\n2.) Visualiser mes groupes\n")
+    print("\n1.) Créer un groupe\n2.) Visualiser mes groupes\n3.) Se déconnecter\n")
 
     choix = int(input("Votre choix : "))
 
     if(choix == 1):
-        creationgroupe.creationGroupe(user)
+        creationgroupe.creationGroupe(utilisateur)
+    elif(choix == 3):
+        utilisateur = None
+        authentification()
 
-
-menuPrincipal(user)
+authentification()
 
 
 connexion.con.close
