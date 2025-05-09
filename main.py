59bf6e0bc915e361d0fcb98d7d67e7ae01cc802a
niakale <diakitetenin99@gmail.com>
2025-05-09 16:41:56 +0000

    Ajoute la fonctionnalité de suppression de membre

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import connexion, createUser, connectUser, models, creationgroupe, sys, administrateur, notification
+import connexion, createUser, connectUser, models, creationgroupe, sys, administrateur, notification, supprimerMembre
 
 connexion.initialize()
 
 def authentification():
     print("\n\n------------- Bienvenue sur Nianaboli, votre console de gestion de dépenses collaboratives -------------\n\n")
     choix = 0
-    while(choix not in (1, 2, 3)):
+    while(choix not in (1, 2, 3, 4)):
         print("1.) Connexion\n2.) Inscription\n3.) Quitter\n")
         choix = int(input("Votre choix : "))
 
@@ -18,7 +18,12 @@ def authentification():
             user = createUser.creationProcess()
         case 3:
             sys.exit("À bientôt!")
-    menuPrincipal(user)
+        case 4:
+            administrateur.viewAllGroups()
+    if(user is None):
+        authentification()
+    else:
+        menuPrincipal(user)
 
 def menuPrincipal(utilisateur: models.UtilisateurInfo):
     print(f"\nBienvenue {utilisateur.prenom} {utilisateur.nom} :)\n")
